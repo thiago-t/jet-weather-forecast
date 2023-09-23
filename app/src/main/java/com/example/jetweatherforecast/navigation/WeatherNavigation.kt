d3cb2a3fbea5b85491b4071c12c96b8fecd40503
Thiago Toazza <thiagotoazza@outlook.com>
2023-09-23 13:38:03 -0300

    Navigation setup; Added simple animated splash screen; Edited .gitignore file

---
+++ app/src/main/java/com/example/jetweatherforecast/navigation/WeatherNavigation.kt
@@ -0,0 +1,22 @@
+package com.example.jetweatherforecast.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.jetweatherforecast.screens.main.MainScreen
+import com.example.jetweatherforecast.screens.splash.WeatherSplashScreen
+
+@Composable
+fun WeatherNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
+        composable(WeatherScreens.SplashScreen.name) {
+            WeatherSplashScreen(navController = navController)
+        }
+
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController = navController)
+        }
+    }
+}
