24427f1cebc42d85fbdaaa4ab96e7f3e39df4301
Thiago Toazza <thiagotoazza@outlook.com>
2023-09-26 21:30:57 -0300

    Added new screen to display info about the app

---
+++ app/src/main/java/com/example/jetweatherforecast/screens/about/AboutScreen.kt
@@ -0,0 +1,64 @@
+package com.example.jetweatherforecast.screens.about
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavController
+import com.example.jetweatherforecast.R
+import com.example.jetweatherforecast.widgets.WeatherAppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            WeatherAppBar(
+                title = "About",
+                icon = Icons.Default.ArrowBack,
+                isMainScreen = false,
+                navController = navController,
+                onAddActionClicked = { }
+            ) {
+                navController.popBackStack()
+            }
+        }
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.about_app),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = stringResource(id = R.string.developed_by),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/jetweatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/jetweatherforecast/widgets/WeatherAppBar.kt
@@ -1,10 +1,23 @@
 package com.example.jetweatherforecast.widgets
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,7 +25,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -20,6 +40,8 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.navigation.WeatherScreens
+import kotlin.math.exp
 
 @ExperimentalMaterial3Api
 @Composable
@@ -32,6 +54,11 @@ fun WeatherAppBar(
     onAddActionClicked: () -> Unit,
     onButtonClicked: () -> Unit
 ) {
+    val showDialog = remember { mutableStateOf(false) }
+
+    if (showDialog.value) {
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+    }
 
     TopAppBar(
         title = {
@@ -46,7 +73,7 @@ fun WeatherAppBar(
                 IconButton(onClick = { onAddActionClicked.invoke() }) {
                     Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
                 }
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { showDialog.value = true }) {
                     Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More icon")
                 }
             } else {
@@ -66,4 +93,59 @@ fun WeatherAppBar(
             }
         },
     )
+}
+
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+    var expanded by remember { mutableStateOf(true) }
+    val items = listOf("About", "Favorites", "Settings")
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)
+        ) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+                        showDialog.value = false
+                    },
+                    text = {
+                        Text(
+                            text,
+                            modifier = Modifier.clickable {
+                                navController.navigate(
+                                    when (text) {
+                                        "About" -> WeatherScreens.AboutScreen.name
+                                        "Favorites" -> WeatherScreens.FavoriteScreen.name
+                                        else -> WeatherScreens.SettingsScreen.name
+                                    }
+                                )
+                            },
+                            fontWeight = FontWeight.Normal
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = when (text) {
+                                "About" -> Icons.Default.Info
+                                "Favorites" -> Icons.Default.Favorite
+                                else -> Icons.Default.Settings
+                            }, contentDescription = null,
+                            tint = Color.LightGray
+                        )
+                    }
+                )
+            }
+        }
+    }
 }
